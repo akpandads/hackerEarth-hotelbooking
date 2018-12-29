24a3c63db2dbf80408617b83d89c0790a996108f
akpandads <39168913+akpandads@users.noreply.github.com>
2018-12-30 01:53:41 +0530

    Enabled cross orgin for localhost 4200 port

--- src/main/java/com/akpanda/hotelbooking/endpoint/HotelRestEndPoint.java
+++ src/main/java/com/akpanda/hotelbooking/endpoint/HotelRestEndPoint.java
@@ -3,13 +3,10 @@ package com.akpanda.hotelbooking.endpoint;
 import com.akpanda.hotelbooking.model.Hotel;
 import com.akpanda.hotelbooking.service.dao.HotelDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/app")
 public class HotelRestEndPoint {
